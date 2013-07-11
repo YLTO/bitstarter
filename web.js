45750574de2794e32f45539f7acacf433691a47c
ylto <yulianto1993@gmail.com>
2013-07-11 12:04:19 +0700

    Modified using buffer instead of string

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// var buffer = new Buffer(30);
+var buffer = fs.readFileSync("index.html");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf8',0,buffer.length););
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
