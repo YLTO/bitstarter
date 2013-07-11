4134f5e2b22aab545f297e2b640986933d53a5f9
ylto <yulianto1993@gmail.com>
2013-07-11 12:14:48 +0700

    correcting error

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 // var buffer = new Buffer(30);
-var buffer = fs.readFileSync("index.html");
+var buffer = fs.readFileSync("/index.html");
 
 
 app.get('/', function(request, response) {
